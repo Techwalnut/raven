563bd99b9f8f2cb1fa879914b0ac8104b9545daa
vrushalin <vrushi.sd@gmail.com>
2014-01-17 18:36:01 +0530

    raven-api

--- raven-api/src/main/java/com/raven/inc/vo/RavenUserVO.java
+++ raven-api/src/main/java/com/raven/inc/vo/RavenUserVO.java
@@ -1,114 +1,112 @@
 package com.raven.inc.vo;
 
 import java.util.Date;
-import java.util.List;
 
 public class RavenUserVO {
-	
+
 	private long id;
 	private String ravenId;
-	private String firstName;
-	private String middleName;
-	private String lastName; 
+	private FbUSerName name;
 	private String cellNo;
 	private String emailId;
 	private Date dob;
 	private String status;
 	private Date createdDate;
 	private Date modifiedDate;
-	private List<RavenFbVO> fbVOs;
+	// private List<RavenFbVO> fbVOs;
 	private String accountType;
-	
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getRavenId() {
 		return ravenId;
 	}
+
 	public void setRavenId(String ravenId) {
 		this.ravenId = ravenId;
 	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getMiddleName() {
-		return middleName;
-	}
-	public void setMiddleName(String middleName) {
-		this.middleName = middleName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+
 	public String getCellNo() {
 		return cellNo;
 	}
+
 	public void setCellNo(String cellNo) {
 		this.cellNo = cellNo;
 	}
+
 	public String getEmailId() {
 		return emailId;
 	}
+
 	public void setEmailId(String emailId) {
 		this.emailId = emailId;
 	}
+
 	public Date getDob() {
 		return dob;
 	}
+
 	public void setDob(Date dob) {
 		this.dob = dob;
 	}
+
 	public String getStatus() {
 		return status;
 	}
+
 	public void setStatus(String status) {
 		this.status = status;
 	}
+
 	public Date getCreatedDate() {
 		return createdDate;
 	}
+
 	public void setCreatedDate(Date createdDate) {
 		this.createdDate = createdDate;
 	}
+
 	public Date getModifiedDate() {
 		return modifiedDate;
 	}
+
 	public void setModifiedDate(Date modifiedDate) {
 		this.modifiedDate = modifiedDate;
 	}
-	public void setFbVOs(List<RavenFbVO> fbVOs) {
-		this.fbVOs = fbVOs;
-	}
-	public List<RavenFbVO> getFbVOs() {
-		return fbVOs;
-	}
+
+	/*
+	 * public void setFbVOs(List<RavenFbVO> fbVOs) { this.fbVOs = fbVOs; }
+	 * public List<RavenFbVO> getFbVOs() { return fbVOs; }
+	 */
 	public void setAccountType(String accountType) {
 		this.accountType = accountType;
 	}
+
 	public String getAccountType() {
 		return accountType;
 	}
+
+	public FbUSerName getName() {
+		return name;
+	}
+
+	public void setName(FbUSerName name) {
+		this.name = name;
+	}
+
 	@Override
 	public String toString() {
-		return "RavenUserVO [id=" + id + ", ravenId=" + ravenId
-				+ ", firstName=" + firstName + ", middleName=" + middleName
-				+ ", lastName=" + lastName + ", cellNo=" + cellNo
-				+ ", emailId=" + emailId + ", dob=" + dob + ", status="
-				+ status + ", createdDate=" + createdDate + ", modifiedDate="
-				+ modifiedDate + ", fbVOs=" + fbVOs + ", accountType="
-				+ accountType + "]";
+		return "RavenUserVO [id=" + id + ", ravenId=" + ravenId + ", name="
+				+ name + ", cellNo=" + cellNo + ", emailId=" + emailId
+				+ ", dob=" + dob + ", status=" + status + ", createdDate="
+				+ createdDate + ", modifiedDate=" + modifiedDate
+				+ ", accountType=" + accountType + "]";
 	}
-	
-	
-
 
 }
