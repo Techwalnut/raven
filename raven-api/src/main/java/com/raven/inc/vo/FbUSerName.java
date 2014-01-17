8a308ea87cb11ec9dd175a7a313377e7e3f74e62
vrushalin <vrushi.sd@gmail.com>
2014-01-17 18:43:27 +0530

    new file

---
+++ raven-api/src/main/java/com/raven/inc/vo/FbUSerName.java
@@ -0,0 +1,39 @@
+package com.raven.inc.vo;
+
+public class FbUSerName {
+
+	private String firstName;
+	private String middleName;
+	private String lastName;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		return "FbUSerName [firstName=" + firstName + ", middleName="
+				+ middleName + ", lastName=" + lastName + "]";
+	}
+
+}
