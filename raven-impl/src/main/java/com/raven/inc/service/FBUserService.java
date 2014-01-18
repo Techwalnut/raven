55111252abe947dcc263fac2a049c942fd92fa84
vrushalin <vrushi.sd@gmail.com>
2014-01-18 16:30:52 +0530

    impl

--- raven-impl/src/main/java/com/raven/inc/service/FBUserService.java
+++ raven-impl/src/main/java/com/raven/inc/service/FBUserService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.raven.inc.manager.FBUserManager;
+import com.raven.inc.utils.ServiceResponse;
 import com.raven.inc.vo.RavenUserVO;
 
 @Service
@@ -12,10 +13,13 @@ public class FBUserService {
 	@Autowired
 	FBUserManager fbUserManager;
 	
-	public RavenUserVO saveFbser(RavenUserVO ravenUserVO) {
+	public ServiceResponse<String> saveFbser(RavenUserVO ravenUserVO) {
 		System.out.println("********Service");
+		
+		ServiceResponse<String> response = new ServiceResponse<String>();
+		
 		fbUserManager.saveFbUser(ravenUserVO);
-		return ravenUserVO;
+		return response;
 		
 	}
 
